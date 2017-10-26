234b205df33e9986fd72efc92967ea8fd69145b5
布衣男儿 <724987481@qq.com>
2017-10-26 23:44:09 +0800

    Update Tmp.m

--- tmp/Tmp.m
+++ tmp/Tmp.m
@@ -3,7 +3,9 @@
 int main(int argc, const char * argv[]) {
    @autoreleasepool {
        // insert code here...
-       NSLog(@"Hello, World!");
+      NSUInteger count = 99;  
+      NSString *objects = @"red balloons";          
+      NSLog(@"Hello%lu, World!%@",count,objects); 
    }
    return 0;
 }
